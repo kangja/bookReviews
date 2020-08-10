51112dea57ee40e663efa087ba7b5d70f47c5051
hi <kangja1996@gmail.com>
2020-08-10 14:28:44 -0400

    authentication is done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :users, only: :create
+  post '/auth/login', to: 'authentication#login'
+  get '/auth/verify', to: 'authentication#verify'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
