6978549a4dbff626c63d4a8e83ae54f86dd16488
hi <kangja1996@gmail.com>
2020-08-11 12:51:11 -0400

    worked on controllers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,21 @@
-class BooksController < ApplicationController
+class BooksController < ApplicationController  
+  before_action :set_book, only: [:show]
+
+  def index
+    @books = Book.all
+    render json: @books, include: :review, status: :ok
+  end
+
+  # GET /books/1
+
+  def show
+    render json: @book, include: :review, status: :ok
+  end
+
+
+  private
+  def set_book
+    @book = Book.find(params[:id])
+  end
+
 end
