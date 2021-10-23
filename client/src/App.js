733fb75c5f0fcdb77bead5536962613848c25ebe
Jason <kangja1996@gmail.com>
2021-10-23 09:28:45 -0400

    add image

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,20 @@
-import React, {Component} from "react"
-import { loginUser, registerUser, removeToken, verifyUser } from './services/auth'
-import Header from "./components/Header"
-import Main from "./components/Main"
-import { withRouter } from 'react-router-dom';
-import './App.css';
+import React, { Component } from "react";
+import {
+  loginUser,
+  registerUser,
+  removeToken,
+  verifyUser,
+} from "./services/auth";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import { withRouter } from "react-router-dom";
+
+import "./App.css";
 
 class App extends Component {
   state = {
     currentUser: null,
-  }
+  };
 
   // as the page first loads, we want to run handleVerify if there is a valid token in local storage we can use. If we have it, we don't need to log in again.
   componentDidMount() {
@@ -17,28 +23,27 @@ class App extends Component {
 
   handleLogin = async (userData) => {
     const currentUser = await loginUser(userData);
-    this.setState({ currentUser })
-  }
+    this.setState({ currentUser });
+  };
 
   handleSignUp = async (userData) => {
     const currentUser = await registerUser(userData);
-    this.setState({ currentUser })
-  }
+    this.setState({ currentUser });
+  };
 
   handleLogout = () => {
     this.setState({
-      currentUser: null
-    })
-    localStorage.removeItem('authToken');
+      currentUser: null,
+    });
+    localStorage.removeItem("authToken");
     removeToken();
-    this.props.history.push('/')
-  }
+    this.props.history.push("/");
+  };
 
   handleVerify = async () => {
     const currentUser = await verifyUser();
     this.setState({ currentUser });
-  }
-
+  };
 
   render() {
     return (
